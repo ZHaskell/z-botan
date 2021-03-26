6abf0dfa5659ae7bcaa2bd9f67b0498b21830e70
alissa-tung <alissa-tung@outlook.com>
2021-03-26 12:19:21 +0800

    wip

--- Z/Crypto/X509.hs
+++ Z/Crypto/X509.hs
@@ -6,12 +6,32 @@
 -- The major certificate format in use today is X.509v3, used for instance in the Transport Layer Security (TLS) protocol. A X.509 certificate is represented by the type X509_Certificate.
 module Z.Crypto.X509 where
 
+import Data.Word (Word64)
+import Z.Botan.Exception (throwBotanIfMinus_)
 import Z.Botan.FFI
-import Z.Crypto.PubKey
-import Z.Crypto.RNG
-import Z.Data.CBytes
+  ( BotanStruct,
+    botan_x509_cert_destroy,
+    botan_x509_cert_dup,
+    botan_x509_cert_gen_selfsigned,
+    botan_x509_cert_get_time_expires,
+    botan_x509_cert_get_time_starts,
+    botan_x509_cert_load_file,
+    botan_x509_cert_not_after,
+    botan_x509_cert_not_before,
+    hs_botan_x509_cert_load,
+    newBotanStruct,
+    withBotanStruct,
+  )
+import Z.Crypto.Hash (HashType)
+import Z.Crypto.PubKey (PrivKey (..))
+import Z.Crypto.RNG (RNG, withRNG)
+import Z.Data.CBytes (CBytes, fromBytes, withCBytesUnsafe)
 import qualified Z.Data.Vector as V
 import Z.Foreign
+  ( allocPrimUnsafe,
+    allocPrimVectorUnsafe,
+    withPrimVectorUnsafe,
+  )
 
 -- | An opaque newtype wrapper for an X.509 certificate.
 newtype X509Cert = X509Cert BotanStruct
@@ -51,3 +71,45 @@ newX509CertSelfsigned (PrivKey key) rng common org = do
       withCBytesUnsafe common $ \common' ->
         withCBytesUnsafe org $ \org' ->
           X509Cert <$> newBotanStruct (\ret -> botan_x509_cert_gen_selfsigned ret key' rng' common' org') botan_x509_cert_destroy
+
+maxTDSize :: Int
+maxTDSize = 16
+
+-- | Return the time the certificate becomes valid, as a CBytes in form “YYYYMMDDHHMMSSZ” where Z is a literal character reflecting that this time is relative to UTC.
+readX509CertStart :: X509Cert -> IO CBytes
+readX509CertStart (X509Cert cert) = do
+  (a, _) <- allocPrimVectorUnsafe maxTDSize $ \td -> do
+    (a', _) <- allocPrimUnsafe @Int $ \len ->
+      withBotanStruct cert $ \cert' ->
+        throwBotanIfMinus_ $ botan_x509_cert_get_time_starts cert' td len
+    pure a'
+  return $ fromBytes a
+
+-- | Return the time the certificate expires, as a CBytes in form “YYYYMMDDHHMMSSZ” where Z is a literal character reflecting that this time is relative to UTC.
+readX509CertExpire :: X509Cert -> IO CBytes
+readX509CertExpire (X509Cert cert) = do
+  (a, _) <- allocPrimVectorUnsafe maxTDSize $ \td -> do
+    (a', _) <- allocPrimUnsafe @Int $ \len ->
+      withBotanStruct cert $ \cert' ->
+        throwBotanIfMinus_ $ botan_x509_cert_get_time_expires cert' td len
+    pure a'
+  return (fromBytes a)
+
+-- | Return the time the certificate becomes valid, as seconds since epoch.
+readX509CertNotBefore :: X509Cert -> IO Word64
+readX509CertNotBefore (X509Cert cert) = do
+  withBotanStruct cert $ \cert' -> do
+    (a, _) <- allocPrimUnsafe @Word64 $ \time ->
+      botan_x509_cert_not_before cert' time
+    return a
+
+-- | Return the time the certificate expires, as seconds since epoch.
+readX509CertNotAfter :: X509Cert -> IO Word64
+readX509CertNotAfter (X509Cert cert) = do
+  withBotanStruct cert $ \cert' -> do
+    (a, _) <- allocPrimUnsafe @Word64 $ \time ->
+      botan_x509_cert_not_after cert' time
+    return a
+
+readX509CertFingerPrint :: X509Cert -> IO HashType
+readX509CertFingerPrint = undefined
