509239bdbbb19e85874de86f9de3e92d02463322
alissa-tung <alissa-tung@outlook.com>
2021-03-26 19:51:42 +0800

    wip

--- Z/Crypto/X509.hs
+++ Z/Crypto/X509.hs
@@ -6,26 +6,34 @@
 -- The major certificate format in use today is X.509v3, used for instance in the Transport Layer Security (TLS) protocol. A X.509 certificate is represented by the type X509_Certificate.
 module Z.Crypto.X509 where
 
-import Data.Word (Word64)
+import Foreign (Word32, Word64)
 import Z.Botan.Exception (throwBotanIfMinus_)
 import Z.Botan.FFI
   ( BotanStruct,
+    botan_pubkey_destroy,
     botan_x509_cert_destroy,
     botan_x509_cert_dup,
     botan_x509_cert_gen_selfsigned,
+    botan_x509_cert_get_authority_key_id,
     botan_x509_cert_get_fingerprint,
+    botan_x509_cert_get_issuer_dn,
+    botan_x509_cert_get_public_key,
+    botan_x509_cert_get_public_key_bits,
     botan_x509_cert_get_serial_number,
+    botan_x509_cert_get_subject_dn,
+    botan_x509_cert_get_subject_key_id,
     botan_x509_cert_get_time_expires,
     botan_x509_cert_get_time_starts,
     botan_x509_cert_load_file,
     botan_x509_cert_not_after,
     botan_x509_cert_not_before,
+    botan_x509_cert_to_string,
     hs_botan_x509_cert_load,
     newBotanStruct,
     withBotanStruct,
   )
 import Z.Crypto.Hash (HashType, hashTypeToCBytes)
-import Z.Crypto.PubKey (PrivKey (..), maxFingerPrintSize)
+import Z.Crypto.PubKey (PrivKey (..), PubKey (..), maxFingerPrintSize)
 import Z.Crypto.RNG (RNG, withRNG)
 import Z.Data.CBytes (CBytes, fromBytes, withCBytesUnsafe)
 import qualified Z.Data.Vector as V
@@ -125,15 +133,123 @@ readX509CertFingerPrint (X509Cert cert) hty = do
             pure a'
           return a
 
-maxSerialSize :: Int
-maxSerialSize = 32
+maxX509ReadSize :: Int
+maxX509ReadSize = 64
 
 -- | Return the serial number of the certificate.
-readX509CertSerialNumber :: X509Cert -> IO V.Bytes
-readX509CertSerialNumber (X509Cert cert) = do
+readX509CertSerial :: X509Cert -> IO V.Bytes
+readX509CertSerial (X509Cert cert) = do
   withBotanStruct cert $ \cert' -> do
-    (a, _) <- allocPrimVectorUnsafe maxSerialSize $ \ret -> do
+    (a, _) <- allocPrimVectorUnsafe maxX509ReadSize $ \ret -> do
       (a', _) <- allocPrimUnsafe @Int $ \len ->
         throwBotanIfMinus_ $ botan_x509_cert_get_serial_number cert' ret len
       pure a'
     return a
+
+-- | Return the authority key ID set in the certificate, which may be empty.
+readX509CertIDAuthority :: X509Cert -> IO V.Bytes
+readX509CertIDAuthority (X509Cert cert) = do
+  withBotanStruct cert $ \cert' -> do
+    (a, _) <- allocPrimVectorUnsafe maxX509ReadSize $ \ret -> do
+      (a', _) <- allocPrimUnsafe @Int $ \len ->
+        throwBotanIfMinus_ $ botan_x509_cert_get_authority_key_id cert' ret len
+      pure a'
+    return a
+
+-- | Return the subject key ID set in the certificate, which may be empty.
+readX509CertIDSubject :: X509Cert -> IO V.Bytes
+readX509CertIDSubject (X509Cert cert) = do
+  withBotanStruct cert $ \cert' -> do
+    (a, _) <- allocPrimVectorUnsafe maxX509ReadSize $ \ret -> do
+      (a', _) <- allocPrimUnsafe @Int $ \len ->
+        throwBotanIfMinus_ $ botan_x509_cert_get_subject_key_id cert' ret len
+      pure a'
+    return a
+
+-- | Get the serialized representation of the public key included in this certificate.
+readX509CertPubBits :: X509Cert -> IO V.Bytes
+readX509CertPubBits (X509Cert cert) = do
+  withBotanStruct cert $ \cert' -> do
+    (a, _) <- allocPrimVectorUnsafe maxX509ReadSize $ \ret -> do
+      (a', _) <- allocPrimUnsafe @Int $ \len ->
+        throwBotanIfMinus_ $ botan_x509_cert_get_public_key_bits cert' ret len
+      pure a'
+    return a
+
+-- | Get the public key included in this certificate.
+readX509CertPub :: X509Cert -> IO PubKey
+readX509CertPub (X509Cert cert) = do
+  withBotanStruct cert $ \cert' ->
+    PubKey <$> newBotanStruct (cert' `botan_x509_cert_get_public_key`) botan_pubkey_destroy
+
+-- | Get a value from the issuer DN field.
+readX509CertDNIssuer ::
+  X509Cert ->
+  -- | key
+  CBytes ->
+  -- | index
+  Int ->
+  IO V.Bytes
+readX509CertDNIssuer (X509Cert cert) key ix = do
+  withBotanStruct cert $ \cert' ->
+    withCBytesUnsafe key $ \key' -> do
+      (a, _) <- allocPrimVectorUnsafe maxX509ReadSize $ \ret -> do
+        (a', _) <- allocPrimUnsafe @Int $ \len ->
+          throwBotanIfMinus_ $ botan_x509_cert_get_issuer_dn cert' key' ix ret len
+        pure a'
+      return a
+
+-- | Get a value from the subject DN field.
+readX509CertDNSubject ::
+  X509Cert ->
+  -- | key
+  CBytes ->
+  -- | index
+  Int ->
+  IO V.Bytes
+readX509CertDNSubject (X509Cert cert) key ix = do
+  withBotanStruct cert $ \cert' ->
+    withCBytesUnsafe key $ \key' -> do
+      (a, _) <- allocPrimVectorUnsafe maxX509ReadSize $ \ret -> do
+        (a', _) <- allocPrimUnsafe @Int $ \len ->
+          throwBotanIfMinus_ $ botan_x509_cert_get_subject_dn cert' key' ix ret len
+        pure a'
+      return a
+
+-- | Format the certificate as a free-form string (CBytes).
+x509CertToCBytes :: X509Cert -> IO CBytes
+x509CertToCBytes (X509Cert cert) = do
+  withBotanStruct cert $ \cert' -> do
+    (a, _) <- allocPrimVectorUnsafe maxX509ReadSize $ \ret -> do
+      (a', _) <- allocPrimUnsafe @Int $ \len ->
+        throwBotanIfMinus_ $ botan_x509_cert_to_string cert' ret len
+      pure a'
+    return $ fromBytes a
+
+-- | Certificate key usage constraints.
+data X509CertKeyConstraint
+  = NoConstraints
+  | DigitalSignatrue
+  | NonRepudiation
+  | KeyEncipherment
+  | DataEncipherment
+  | KeyAgreement
+  | KeyCertSign
+  | CrlSign
+  | EncipherOnly
+  | DecipherOnly
+
+constraintToWord32 :: X509CertKeyConstraint -> Word32 -- unsigned int key_usage
+constraintToWord32 = \case
+  NoConstraints -> 0
+  DigitalSignatrue -> 32768
+  NonRepudiation -> 16384
+  KeyEncipherment -> 8192
+  DataEncipherment -> 4096
+  KeyAgreement -> 2048
+  KeyCertSign -> 1024
+  CrlSign -> 512
+  EncipherOnly -> 256
+  DecipherOnly -> 128
+
+type X509CertUsage = (X509Cert, X509CertKeyConstraint)
