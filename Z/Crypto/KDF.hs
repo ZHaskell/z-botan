2481ae84fc9cd901f22d6f591302b27015a801e7
Tao He <sighingnow@gmail.com>
2021-04-13 00:08:18 +0800

    Fixes KDF to set MAC as parameter as well.
    Make it consistent with the implementation of botan.
    
    Signed-off-by: Tao He <sighingnow@gmail.com>

--- Z/Crypto/KDF.hs
+++ Z/Crypto/KDF.hs
@@ -15,6 +15,7 @@ module Z.Crypto.KDF (
     KDFType(..)
   , BlockCipherType (..)
   , HashType(..)
+  , MACType(..)
   , kdf
   , kdf'
   -- * PBKDF
@@ -22,12 +23,13 @@ module Z.Crypto.KDF (
   , pbkdf
   , pbkdfTimed
   -- * Internal helps
-  , kDFTypeToCBytes
-  , pBKDFTypeToParam
+  , kdfTypeToCBytes
+  , pbkdfTypeToParam
   ) where
 
-import Z.Crypto.Cipher (BlockCipherType (..), blockCipherTypeToCBytes)
-import Z.Crypto.Hash
+import Z.Crypto.Cipher (BlockCipherType (..))
+import Z.Crypto.Hash (HashType (..), hashTypeToCBytes)
+import Z.Crypto.MAC (MACType (..), macTypeToCBytes)
 import Z.Botan.FFI
 import Z.Data.CBytes (CBytes, withCBytesUnsafe, withCBytes)
 import qualified Z.Data.CBytes as CB
@@ -39,11 +41,13 @@ import Z.Botan.Exception
 -- Key Derivation Function --
 -----------------------------
 
--- | Key derivation functions are used to turn some amount of shared secret material into uniform random keys suitable for use with symmetric algorithms. An example of an input which is useful for a KDF is a shared secret created using Diffie-Hellman key agreement.
+-- | Key derivation functions are used to turn some amount of shared secret material into uniform random keys
+-- suitable for use with symmetric algorithms. An example of an input which is useful for a KDF is a shared
+-- secret created using Diffie-Hellman key agreement.
 data KDFType
-    = HKDF HashType
-    | HKDF_Extract HashType
-    | HKDF_Expand HashType
+    = HKDF MACType
+    | HKDF_Extract MACType
+    | HKDF_Expand MACType
     -- ^ Defined in RFC 5869, HKDF uses HMAC to process inputs.
     -- Also available are variants HKDF-Extract and HKDF-Expand.
     -- HKDF is the combined Extract+Expand operation.
@@ -56,37 +60,31 @@ data KDFType
     -- ^ KDF1 from IEEE 1363. It can only produce an output at most the length of the hash function used.
     | TLS_PRF
     -- ^
-    | TLS_12_PRF HashType
-    | SP800_108_Counter HashType
-    | SP800_108_Counter' BlockCipherType
-    | SP800_108_Feedback HashType
-    | SP800_108_Feedback' BlockCipherType
-    | SP800_108_Pipeline HashType
-    | SP800_108_Pipeline' BlockCipherType
+    | TLS_12_PRF MACType
+    | SP800_108_Counter MACType
+    | SP800_108_Feedback MACType
+    | SP800_108_Pipeline MACType
     | SP800_56AHash HashType
     -- ^ NIST SP 800-56A KDF using hash function
-    | SP800_56AHMAC HashType
+    | SP800_56AMAC MACType
     -- ^ NIST SP 800-56A KDF using HMAC
-    | SP800_56C HashType
+    | SP800_56C MACType
 
-kDFTypeToCBytes :: KDFType -> CBytes
-kDFTypeToCBytes (HKDF ht        ) = CB.concat [ "HKDF(" , hashTypeToCBytes ht, ")"]
-kDFTypeToCBytes (HKDF_Extract ht) = CB.concat [ "HKDF-Extract(" , hashTypeToCBytes ht, ")"]
-kDFTypeToCBytes (HKDF_Expand ht ) = CB.concat [ "HKDF-Expand(" , hashTypeToCBytes ht, ")"]
-kDFTypeToCBytes (KDF2 ht        ) = CB.concat [ "KDF2(" , hashTypeToCBytes ht, ")"]
-kDFTypeToCBytes (KDF1_18033 ht  ) = CB.concat [ "KDF1-18033(" , hashTypeToCBytes ht, ")"]
-kDFTypeToCBytes (KDF1 ht        ) = CB.concat [ "KDF1(" , hashTypeToCBytes ht, ")"]
-kDFTypeToCBytes (TLS_PRF        ) = "TLS-PRF"
-kDFTypeToCBytes (TLS_12_PRF ht  ) = CB.concat [ "TLS-12-PRF(" , hashTypeToCBytes ht, ")"]
-kDFTypeToCBytes (SP800_108_Counter ht  ) = CB.concat [ "SP800-108-Counter(HMAC(" , hashTypeToCBytes ht, "))"]
-kDFTypeToCBytes (SP800_108_Counter' ct ) = CB.concat [ "SP800-108-Counter(CMAC(" , blockCipherTypeToCBytes ct, "))"]
-kDFTypeToCBytes (SP800_108_Feedback ht ) = CB.concat [ "SP800-108-Feedback(HMAC(" , hashTypeToCBytes ht, "))"]
-kDFTypeToCBytes (SP800_108_Feedback' ct) = CB.concat [ "SP800-108-Feedback(CMAC(" , blockCipherTypeToCBytes ct, "))"]
-kDFTypeToCBytes (SP800_108_Pipeline ht ) = CB.concat [ "SP800-108-Pipeline(HMAC(" , hashTypeToCBytes ht, "))"]
-kDFTypeToCBytes (SP800_108_Pipeline' ct) = CB.concat [ "SP800-108-Pipeline(CMAC(" , blockCipherTypeToCBytes ct, "))"]
-kDFTypeToCBytes (SP800_56AHash ht      ) = CB.concat [ "SP800-56A(" , hashTypeToCBytes ht, ")"]
-kDFTypeToCBytes (SP800_56AHMAC ht      ) = CB.concat [ "SP800-56A(HMAC(" , hashTypeToCBytes ht, "))"]
-kDFTypeToCBytes (SP800_56C ht          ) = CB.concat [ "SP800-56C(" , hashTypeToCBytes ht, ")"]
+kdfTypeToCBytes :: KDFType -> CBytes
+kdfTypeToCBytes (HKDF mt        ) = CB.concat [ "HKDF(" , macTypeToCBytes mt, ")"]
+kdfTypeToCBytes (HKDF_Extract mt) = CB.concat [ "HKDF-Extract(" , macTypeToCBytes mt, ")"]
+kdfTypeToCBytes (HKDF_Expand mt ) = CB.concat [ "HKDF-Expand(" , macTypeToCBytes mt, ")"]
+kdfTypeToCBytes (KDF2 ht        ) = CB.concat [ "KDF2(" , hashTypeToCBytes ht, ")"]
+kdfTypeToCBytes (KDF1_18033 ht  ) = CB.concat [ "KDF1-18033(" , hashTypeToCBytes ht, ")"]
+kdfTypeToCBytes (KDF1 ht        ) = CB.concat [ "KDF1(" , hashTypeToCBytes ht, ")"]
+kdfTypeToCBytes (TLS_PRF        ) = "TLS-PRF"
+kdfTypeToCBytes (TLS_12_PRF mt  ) = CB.concat [ "TLS-12-PRF(" , macTypeToCBytes mt, ")"]
+kdfTypeToCBytes (SP800_108_Counter mt ) = CB.concat [ "SP800-108-Counter(" ,  macTypeToCBytes mt, ")"]
+kdfTypeToCBytes (SP800_108_Feedback mt) = CB.concat [ "SP800-108-Feedback(" , macTypeToCBytes mt, ")"]
+kdfTypeToCBytes (SP800_108_Pipeline mt) = CB.concat [ "SP800-108-Pipeline(" , macTypeToCBytes mt, ")"]
+kdfTypeToCBytes (SP800_56AHash ht     ) = CB.concat [ "SP800-56A(" , hashTypeToCBytes ht, ")"]
+kdfTypeToCBytes (SP800_56AMAC mt      ) = CB.concat [ "SP800-56A(" , macTypeToCBytes mt, ")"]
+kdfTypeToCBytes (SP800_56C mt         ) = CB.concat [ "SP800-56C(" , macTypeToCBytes mt, ")"]
 
 -- | Derive a key using the given KDF algorithm.
 kdf
@@ -97,7 +95,7 @@ kdf
   -> V.Bytes    -- ^ label
   -> IO V.Bytes
 kdf algo siz secret salt label =
-    withCBytesUnsafe (kDFTypeToCBytes algo) $ \ algoBA ->
+    withCBytesUnsafe (kdfTypeToCBytes algo) $ \ algoBA ->
         withPrimVectorUnsafe secret $ \ secretBA secretOff secretLen ->
             withPrimVectorUnsafe salt $ \ saltBA saltOff saltLen ->
                 withPrimVectorUnsafe label $ \ labelBA labelOff labelLen ->
@@ -122,14 +120,13 @@ kdf' algo siz secret = kdf algo siz secret mempty mempty
 -- Password-Based Key Derivation Function --
 --------------------------------------------
 
--- | Often one needs to convert a human readable password into a cryptographic key. It is useful to slow down the computation of these computations in order to reduce the speed of brute force search, thus they are parameterized in some way which allows their required computation to be tuned.
+-- | Often one needs to convert a human readable password into a cryptographic key. It is useful to slow down the
+-- computation of these computations in order to reduce the speed of brute force search, thus they are parameterized
+-- in some way which allows their required computation to be tuned.
 data PBKDFType
-    = PBKDF2 HashType Int   -- ^ iterations
+    = PBKDF2 MACType Int   -- ^ iterations
     -- ^ PBKDF2 is the “standard” password derivation scheme,
-    -- widely implemented in many different libraries. It uses HMAC internally.
-    | PBKDF2' BlockCipherType Int   -- ^ iterations
-    -- ^ PBKDF2 is the “standard” password derivation scheme,
-    -- widely implemented in many different libraries. It uses HMAC internally.
+    -- widely implemented in many different libraries.
     | Scrypt  Int Int Int   -- ^ N, r, p
     -- ^ Scrypt is a relatively newer design which is “memory hard”,
     -- in addition to requiring large amounts of CPU power it uses a large block of memory to compute the hash.
@@ -142,15 +139,14 @@ data PBKDFType
     | OpenPGP_S2K HashType Int -- ^ iterations
     -- ^ The OpenPGP algorithm is weak and strange, and should be avoided unless implementing OpenPGP.
 
-pBKDFTypeToParam :: PBKDFType -> (CBytes, Int, Int, Int)
-pBKDFTypeToParam (PBKDF2 ht i     ) = (CB.concat [ "PBKDF2(HMAC(" , hashTypeToCBytes ht, "))"], i, 0, 0)
-pBKDFTypeToParam (PBKDF2' ct i    ) = (CB.concat [ "PBKDF2(CMAC(" , blockCipherTypeToCBytes ct, "))"], i, 0, 0)
-pBKDFTypeToParam (Scrypt n r p    ) = ("Scrypt", n, r, p)
-pBKDFTypeToParam (Argon2d i m p   ) = ("Argon2d", i, m, p)
-pBKDFTypeToParam (Argon2i i m p   ) = ("Argon2i", i, m, p)
-pBKDFTypeToParam (Argon2id i m p  ) = ("Argon2id", i, m, p)
-pBKDFTypeToParam (Bcrypt i        ) = ("Bcrypt-PBKDF", i, 0, 0)
-pBKDFTypeToParam (OpenPGP_S2K ht i) = (CB.concat [ "OpenPGP-S2K(" , hashTypeToCBytes ht, ")"], i, 0, 0)
+pbkdfTypeToParam :: PBKDFType -> (CBytes, Int, Int, Int)
+pbkdfTypeToParam (PBKDF2 mt i     ) = (CB.concat [ "PBKDF2(" , macTypeToCBytes mt, ")"], i, 0, 0)
+pbkdfTypeToParam (Scrypt n r p    ) = ("Scrypt", n, r, p)
+pbkdfTypeToParam (Argon2d i m p   ) = ("Argon2d", i, m, p)
+pbkdfTypeToParam (Argon2i i m p   ) = ("Argon2i", i, m, p)
+pbkdfTypeToParam (Argon2id i m p  ) = ("Argon2id", i, m, p)
+pbkdfTypeToParam (Bcrypt i        ) = ("Bcrypt-PBKDF", i, 0, 0)
+pbkdfTypeToParam (OpenPGP_S2K ht i) = (CB.concat [ "OpenPGP-S2K(" , hashTypeToCBytes ht, ")"], i, 0, 0)
 
 -- | Derive a key from a passphrase for a number of iterations using the given PBKDF algorithm and params.
 pbkdf :: PBKDFType  -- ^ PBKDF algorithm type
@@ -177,9 +173,10 @@ pbkdf typ siz pwd salt = do
                             pwdBA ppOff ppLen
                             saltBA saltOff saltLen)
   where
-    (algo, i1, i2, i3) = pBKDFTypeToParam typ
+    (algo, i1, i2, i3) = pbkdfTypeToParam typ
 
--- | Derive a key from a passphrase using the given PBKDF algorithm, the iteration params are ignored and PBKDF is run until given milliseconds have passed.
+-- | Derive a key from a passphrase using the given PBKDF algorithm, the iteration params are
+-- ignored and PBKDF is run until given milliseconds have passed.
 pbkdfTimed :: PBKDFType  -- ^ the name of the given PBKDF algorithm
            -> Int        -- ^ run until milliseconds have passwd
            -> Int        -- ^ length of output key
@@ -216,4 +213,4 @@ pbkdfTimed typ msec siz pwd s = do
                             pwd' ppOff ppLen
                             s' sOff sLen)
   where
-    (algo, _, _, _) = pBKDFTypeToParam typ
+    (algo, _, _, _) = pbkdfTypeToParam typ
