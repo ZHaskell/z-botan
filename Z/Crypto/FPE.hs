5c4ed131f4d6d9e86211dcf165372eb61a4a6e6d
alissa-tung <alissa-tung@outlook.com>
2021-05-09 22:52:42 +0800

    modify otp

---
+++ Z/Crypto/FPE.hs
@@ -0,0 +1,1 @@
+module Z.Crypto.FPE where
--- Z/Crypto/OTP.hs
+++ Z/Crypto/OTP.hs
@@ -1,5 +1,6 @@
 module Z.Crypto.OTP where
 
+import           Data.Word
 import           Z.Botan.FFI
 import           Z.Data.CBytes
 import qualified Z.Data.Vector as V
@@ -9,14 +10,34 @@ newtype HOTP = HOTP BotanStruct
 
 newHOTP :: V.Bytes -> CBytes -> Int -> IO HOTP
 newHOTP key hashAlgo digits = do
-      withPrimVectorUnsafe key $ \ key' keyOff keyLen ->
+    withPrimVectorUnsafe key $ \ key' keyOff keyLen ->
         withCBytesUnsafe hashAlgo $ \ hashAlgo' ->
-          HOTP <$> newBotanStruct (\ hotp -> hs_botan_hotp_init hotp key' keyOff keyLen hashAlgo' digits) botan_hotp_destroy
+            HOTP <$> newBotanStruct (\ hotp -> hs_botan_hotp_init hotp key' keyOff keyLen hashAlgo' digits) botan_hotp_destroy
+
+-- | Generate a HOTP code for the provided counter.
+genHOTP :: HOTP   -- ^ the HOTP object
+        -> Word64 -- ^ counter
+        -> IO Word32
+genHOTP (HOTP hotp) counter = do
+    withBotanStruct hotp $ \ hotp' -> do
+        (a,_) <- allocPrimUnsafe $ \ code ->
+            botan_hotp_generate hotp' code counter
+        return a
 
 newtype TOTP = TOTP BotanStruct
 
 newTOTP :: V.Bytes -> CBytes -> Int -> Int -> IO TOTP
 newTOTP key hashAlgo digits timeStep = do
-      withPrimVectorUnsafe key $ \ key' keyOff keyLen ->
+    withPrimVectorUnsafe key $ \ key' keyOff keyLen ->
         withCBytesUnsafe hashAlgo $ \ hashAlgo' ->
-          TOTP <$> newBotanStruct (\ hotp -> hs_botan_totp_init hotp key' keyOff keyLen hashAlgo' digits timeStep) botan_totp_destroy
+            TOTP <$> newBotanStruct (\ hotp -> hs_botan_totp_init hotp key' keyOff keyLen hashAlgo' digits timeStep) botan_totp_destroy
+
+-- | Generate a TOTP code for the provided timestamp.
+genTOTP :: TOTP   -- ^ the TOTP object
+        -> Word64 -- ^ the current local timestamp
+        -> IO Word32
+genTOTP (TOTP totp) timestamp = do
+    withBotanStruct totp $ \ totp' -> do
+        (a, _) <- allocPrimUnsafe $ \ code ->
+            botan_totp_generate totp' code timestamp
+        return a
