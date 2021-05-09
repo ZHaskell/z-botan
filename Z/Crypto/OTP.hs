966cb2f9ce52fea692cf689ff63c33ae81c35b4a
alissa-tung <alissa-tung@outlook.com>
2021-05-09 10:30:45 +0800

    before merge

--- Z/Crypto/OTP.hs
+++ Z/Crypto/OTP.hs
@@ -12,3 +12,11 @@ newHOTP key hashAlgo digits = do
       withPrimVectorUnsafe key $ \ key' keyOff keyLen ->
         withCBytesUnsafe hashAlgo $ \ hashAlgo' ->
           HOTP <$> newBotanStruct (\ hotp -> hs_botan_hotp_init hotp key' keyOff keyLen hashAlgo' digits) botan_hotp_destroy
+
+newtype TOTP = TOTP BotanStruct
+
+newTOTP :: V.Bytes -> CBytes -> Int -> Int -> IO TOTP
+newTOTP key hashAlgo digits timeStep = do
+      withPrimVectorUnsafe key $ \ key' keyOff keyLen ->
+        withCBytesUnsafe hashAlgo $ \ hashAlgo' ->
+          TOTP <$> newBotanStruct (\ hotp -> hs_botan_totp_init hotp key' keyOff keyLen hashAlgo' digits timeStep) botan_totp_destroy
