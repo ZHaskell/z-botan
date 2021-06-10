fd5f7fb946531a5c163e7a5b2f27e87d7c0d43fe
Dong Han <winterland1989@gmail.com>
2021-06-10 17:20:57 +0800

    fix #6

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,27 @@
 -- file test/Spec.hs
+{-# LANGUAGE CPP #-}
+#ifdef mingw32_HOST_OS
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
+module Main where
+
+import           Control.Monad
+import           Test.Hspec
+import           Test.HUnit
+import           Z.IO
+import qualified Z.IO.FileSystem    as FS
+import qualified Z.Data.Text        as T
+import           Z.Crypto.Hash
+import           Utils
+
+main :: IO ()
+main = hspec $
+    describe "Crypto.Hash" $ do
+        it "SHA256" $ do
+            hash SHA256 "hello, world" @?= "09CA7E4EAA6E8AE9C7D261167129184883644D07DFBA7CBFBC4C8A2E08360D5B"
+
+#else
 {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+#endif
