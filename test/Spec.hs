ee1cd2ce8b7aa0dfb5e18655ca1b998760b6feda
Dong Han <winterland1989@gmail.com>
2021-06-10 18:50:43 +0800

    fix #8

--- test/Spec.hs
+++ test/Spec.hs
@@ -21,6 +21,9 @@ main = hspec $
     describe "Crypto.Hash" $ do
         it "SHA256" $ print "test"
 
+        it "SHA256'" $ do
+            hash SHA256 "hello, world" @?= "09CA7E4EAA6E8AE9C7D261167129184883644D07DFBA7CBFBC4C8A2E08360D5B"
+
 #else
 {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
 #endif
