3d5bd15ab5bce1b40caaf18034a8db2fb83871f5
Dong Han <winterland1989@gmail.com>
2021-06-10 19:36:46 +0800

    fix #9

--- test/Spec.hs
+++ test/Spec.hs
@@ -13,6 +13,7 @@ import           Test.HUnit
 import           Z.IO
 import qualified Z.IO.FileSystem    as FS
 import qualified Z.Data.Text        as T
+import           Z.Crypto.Cipher
 import           Z.Crypto.Hash
 import           Utils
 
@@ -21,8 +22,43 @@ main = hspec $
     describe "Crypto.Hash" $ do
         it "SHA256" $ print "test"
 
-        it "SHA256'" $ do
-            hash SHA256 "hello, world" @?= "09CA7E4EAA6E8AE9C7D261167129184883644D07DFBA7CBFBC4C8A2E08360D5B"
+        forM_
+            [ ("aes.vec", "AES-128", AES128)
+            , ("aes.vec", "AES-192", AES192)
+            , ("aes.vec", "AES-256", AES256)
+            , ("aria.vec", "ARIA-128", ARIA128)
+            , ("aria.vec", "ARIA-192", ARIA192)
+            , ("aria.vec", "ARIA-256", ARIA256)
+            , ("blowfish.vec", "Blowfish", Blowfish)
+            , ("camellia.vec", "Camellia-128", Camellia128)
+            , ("camellia.vec", "Camellia-192", Camellia192)
+            , ("camellia.vec", "Camellia-256", Camellia256)
+            , ("cascade.vec", "Cascade(Serpent,Twofish)", Cascade Serpent Twofish)
+            , ("cascade.vec", "Cascade(Serpent,AES-256)", Cascade Serpent AES256)
+            , ("cascade.vec", "Cascade(Serpent,CAST-128)", Cascade Serpent CAST128)
+            , ("cast128.vec", "CAST-128", CAST128)
+            , ("cast256.vec", "CAST-256", CAST256)
+            , ("des.vec", "DES", DES)
+            , ("idea.vec", "IDEA", IDEA)
+            , ("kasumi.vec", "KASUMI", KASUMI)
+            , ("lion.vec", "Lion(SHA-160,RC4,64)", Lion SHA160 RC4 64)
+            , ("misty.vec", "MISTY1", MISTY1)
+            , ("noekeon.vec", "Noekeon", Noekeon)
+            , ("seed.vec", "SEED", SEED)
+            , ("serpent.vec", "Serpent", Serpent)
+            , ("shacal2.vec", "SHACAL2", SHACAL2)
+            -- dont support iterations
+            -- , ("sm4.vec", "SM4", SM4)
+            -- dont support tweak
+            -- , ("threefish.vec", "Threefish-512", Threefish512)
+            , ("twofish.vec", "Twofish", Twofish)
+            , ("xtea.vec", "XTEA", XTEA)
+            ] $ \ (file, algoName, cipherType) ->
+                it (T.unpack $ T.validate algoName) $ do
+                    tvMap <- parseBlockCipherTestVector =<< "./third_party/botan/src/tests/data/block/" `FS.join` file
+                    tvs <- unwrap' "ENOTFOUND" "no algo founded" $ lookup algoName tvMap
+                    forM_ tvs $ \ (key0, i, o) -> do
+                        print (key0, i, o)
 
 #else
 {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
