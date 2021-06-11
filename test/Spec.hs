401a8d1b29686c57aab7e38b0f4232fcbae42928
Dong Han <winterland1989@gmail.com>
2021-06-11 13:07:37 +0800

    minimize build

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,12 +6,11 @@ import           Test.Hspec
 import           Test.HUnit
 import           Z.IO
 import qualified Z.Data.CBytes      as CB
-import           Z.Botan.FFI
+import           Z.Crypto.MPI
 import           Utils
 
 main :: IO ()
 main = hspec $ do
     describe "test without botan struct" $ do
         it "test" $ do
-            msg <- CB.fromCString =<< botan_x509_cert_validation_status 1
-            msg @?= "OCSP response accepted as affirming unrevoked status for certificate"
+            (1 :: MPI) @?= (1 :: MPI)
